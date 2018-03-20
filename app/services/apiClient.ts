a360052336624cb159f46e990e826d662e7da4da
Dominik Prokop <dominik@fortedigital.no>
2018-03-20 22:58:38 +0100

    Tslint bump

--- app/services/apiClient.ts
+++
@@ -1,8 +0,0 @@
-import ApiClient from 'data/client';
-
-const apiUrl = process.env.API_URL || 'http://localhost:5000';
-const endpointUrl = `${apiUrl}/api/endpoint`;
-
-let apiClient = new ApiClient(endpointUrl);
-
-export default apiClient;
